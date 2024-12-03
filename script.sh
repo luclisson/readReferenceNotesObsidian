df7122e98f06efbfe68243ae59f2b2cb43f28e26
Luc <137832529+luclisson@users.noreply.github.com>
2024-12-03 12:26:22 +0100

    Add files via upload
    run this instead of the main.py to run the program without any error (lets hope so)

---
+++ script.sh
@@ -0,0 +1,11 @@
+
+# change var1 to your obsidian vault reference notes folder
+# change var 2 to your highlighted notes txt file
+VAR1="/Users/lucliss/Documents/second brain/notes/reference notes/"
+VAR2="/Users/lucliss/Downloads/notes.txt"
+
+PYTHON_SCRIPT_PATH="/Users/lucliss/Documents/GitHub/readReferenceNotesPRV/main.py"
+
+python3 "$PYTHON_SCRIPT_PATH" "$VAR1" "$VAR2"
+
+#enjoy referencing :)
